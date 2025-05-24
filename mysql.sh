6f75fc63fb27d6bd7a0005411d0a2347a50b4a39
JoinDevops0425 <joindevops0425@gmail.com>
2025-05-24 23:45:00 +0530

    SHell Project 2

--- mysql.sh
+++ mysql.sh
@@ -1,41 +1,7 @@
 #!/bin/bash
-START_TIME=$(date +%s)
-
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-
-
-SCRIPT_DIR=$PWD
-
-mkdir -p $LOGS_FOLDER
-echo "Script started executeing at : $(date)" | tee -a $LOG_FILE
-
-if [ $USERID -ne 0 ]
-then 
-    echo -e "$R Error : Please run as a root user$N" | tee -a $LOG_FILE
-    exit 1
-else 
-    echo "You are running as a root user" | tee -a $LOG_FILE
-fi
-
-VALIDATE(){
-
-    if [ $1 -eq 0 ]
-    then 
-        echo -e "$2 is $G susccessful$N" | tee -a $LOG_FILE
-    else 
-        echo -e "$2 is $R failed$N" | tee -a $LOG_FILE
-        exit 1
-    fi    
-}
-
+source ./common.sh
+app_name="mysql"
+CHECK_ROOT
 dnf install mysql-server -y &>>$LOG_FILE   
 VALIDATE $? "Installing MySQL Server"
 
@@ -47,3 +13,5 @@ VALIDATE $? "Starting MySQL Server"
 
 mysql_secure_installation --set-root-pass RoboShop@1 &>>$LOG_FILE
 VALIDATE $? "Setting MySQL root password"
+
+PRITN_TIME
