3417cf69cb3ecef7a11e557d0937078579b4e373
JoinDevops0425 <joindevops0425@gmail.com>
2025-05-24 23:13:36 +0530

    SHell Project 2

--- catalogue.sh
+++ catalogue.sh
@@ -1,81 +1,12 @@
 #!/bin/bash
 
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
+source ./common.sh
+app_name="catalogue"
 
-LOG_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE=$LOG_FOLDER/$SCRIPT_NAME.log
-SCRIPT_DIR=$PWD 
-
-mkdir -p $LOG_FOLDER
-
-echo "Script started executing at : $(date)" | tee -a $LOG_FILE
-
-if [ $USERID -ne 0 ]
-then 
-    echo -e "$R Error : $N please run as a root user" | tee -a $LOG_FILE
-    exit 1
-else 
-    echo "You are running as a root user" | tee -a $LOG_FILE
-fi
-
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then 
-        echo -e "$2 is ${G} Successfull$N" | tee -a $LOG_FILE
-    else 
-        echo "$2 is $R failed$N" | tee -a $LOG_FILE
-        exit 1
-    fi
-}
-
-dnf module disable nodejs -y &>>LOG_FILE
-VALIDATE $? "Disabling Nodejs Default version"
-
-dnf module enable nodejs:20 -y &>>LOG_FILE
-VALIDATE $? "Enablling Nodejs version 20"
-
-dnf install nodejs -y &>>LOG_FILE
-VALIDATE $? "Installing Nodejs"
-
-mkdir -p /opt/app
-VALIDATE $? "Creating App Directory"
-
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip 
-VALIDATE $? "Downloading catalogue.zip"
-
-rm -rf /opt/app/*
-
-cd /opt/app
-unzip /tmp/catalogue.zip &>>LOG_FILE
-VALIDATE $? "Unzipping catalogue.zip"
-
-npm install &>>LOG_FILE
-VALIDATE $? "Installing Nodejs dependencies"
-
-id roboshop 
-if [ $? -ne 0 ]
-then 
-    useradd --system --home /opt/app --shell /sbin/nologin --comment "System User" roboshop
-    VALIDATE $? "Creating roboshop user"
-else 
-    echo "User Roboshop already exixts"
-fi
-
-
-cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
-VALIDATE $? "Copying catalogue.service file"
-
-systemctl daemon-reload &>>LOG_FILE
-
-systemctl enable catalogue &>>LOG_FILE
-VALIDATE $? "Enabling catalogue service"
-
-systemctl start catalogue &>>LOG_FILE
+CHECK_ROOT
+APP_SETUP
+NODEJS_SETUP
+SYSTEMD_SETUP
 
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongod.repo
 VALIDATE $? "Copying MongoDB Repo file"
@@ -93,3 +24,4 @@ else
     echo "Catalogue database already exists"
 fi    
 
+PRINT_TIME
